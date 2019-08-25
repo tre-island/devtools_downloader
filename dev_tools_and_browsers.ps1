97c1120998934f274bf39289313fc7ea85dfe3e9
treprime <45750898+treprime@users.noreply.github.com>
2019-08-25 11:37:31 -0600

    uploaded script to dev coding tools and browsers
    installs the following:
    scoop
    git
    firefox
    googlechrome
    7zip
    atom
    vscode
    eclipse-java
    jetbrains
    IntelliJ-IDEA

---
+++ dev_tools_and_browsers.ps1
@@ -0,0 +1,23 @@
+
+Set-ExecutionPolicy RemoteSigned -scope CurrentUser
+
+TRY
+{
+	scoop --version
+}
+Catch
+{
+	iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
+
+	scoop install git
+}
+
+scoop bucket add extras
+scoop install firefox
+scoop install googlechrome
+scoop install 7zip
+scoop install atom
+scoop install vscode
+scoop install eclipse-java
+scoop bucket add jetbrains
+scoop install IntelliJ-IDEA
